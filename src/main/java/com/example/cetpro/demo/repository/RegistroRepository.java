5ef7e04ca1b1ae98fde4cc2146aea3a066ea8936
Junior Salinas <juniorsalinas200022@gmail.com>
2025-10-08 15:13:10 -0500

    Se modifica logica para registrar asistencia y modifica front

--- src/main/java/com/example/cetpro/demo/repository/RegistroRepository.java
+++ src/main/java/com/example/cetpro/demo/repository/RegistroRepository.java
@@ -1,9 +1,13 @@
 package com.example.cetpro.demo.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.cetpro.demo.entity.RegistroEntidad;
 
 public interface RegistroRepository extends JpaRepository<RegistroEntidad, Long>  {
-
+    boolean existsByDni(String dni);
+    Optional<RegistroEntidad> findTopByDniOrderByIdDesc(String dni);
 }
--- src/main/java/com/example/cetpro/demo/rest/AsistenciaAlumno.java
+++ src/main/java/com/example/cetpro/demo/rest/AsistenciaAlumno.java
@@ -11,7 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cetpro.demo.entity.AsistenciaAlumnoEntity;
+import com.example.cetpro.demo.entity.ConfigFlags;
+import com.example.cetpro.demo.entity.RegistroEntidad;
 import com.example.cetpro.demo.repository.AsistenciaAlumnoRepository;
+import com.example.cetpro.demo.repository.ConfigFlagRepository;
+import com.example.cetpro.demo.repository.RegistroRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -20,19 +27,48 @@ public class AsistenciaAlumno {
 
     @Autowired
     private AsistenciaAlumnoRepository asistenciaAlumnoRepository;
+    @Autowired
+    private RegistroRepository registroRepository;
+    @Autowired
+    private ConfigFlagRepository configFlagRepository;
 
     @PostMapping("/guardar")
     public Map<String, String> guardarQR(@RequestBody Map<String, String> datos) {
         String contenido = datos.get("contenido");
+        Boolean existeDni = registroRepository.existsByDni(contenido);
+        boolean permitido = configFlagRepository
+                .findByNombre("acceso_logica")
+                .map(ConfigFlags::getHabilitado)
+                .orElse(false); 
+        String as = configFlagRepository
+                .findByNombre("acceso_logica").get().getAccountsid();
+        String at = configFlagRepository
+                .findByNombre("acceso_logica").get().getAuthtok();
+        String nombre = registroRepository.findTopByDniOrderByIdDesc(contenido).map(RegistroEntidad::getApellidosNombres)
+        .orElse("No encontrado");;
+    
         if (contenido == null || contenido.isBlank()) {
             return Map.of("mensaje", "Registro vacio");
         }
-        System.out.println("📩 Contenido recibido: " + contenido);
-
+        if (!existeDni) {
+            return Map.of("mensaje", "El DNI ingresado no existe!");
+        }
+        
         AsistenciaAlumnoEntity qr = new AsistenciaAlumnoEntity();
         qr.setContenido(contenido);
         asistenciaAlumnoRepository.save(qr);
 
-        return Map.of("mensaje", "Registro guardado exitosamente");
+        //Logica para enviar mensaje cuando el alumno asistio al colegio
+        if(permitido){
+            Twilio.init(as, at);
+            Message messageMaster = Message.creator(
+              new PhoneNumber("whatsapp:+51997315856"),
+              new PhoneNumber("whatsapp:+14155238886"),
+              "El alumno " + nombre + "("+ contenido + ")" + " acaba de registrar asistencia 😁").create();
+                    
+           return Map.of("mensaje", "Registro guardado exitosamente");
+        }else{
+            return Map.of("mensaje", "No se envio mensaje por flag");
+        }
     }
 }
